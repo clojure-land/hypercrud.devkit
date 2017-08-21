373a1b098b12b44f6198bff6a2d3daa3d81f3571
Karl Hardenstine <khardenstine@gmail.com>
2017-08-21 19:50:36 -0400

    Use root-init/init to bootstrap root

--- hello-world/service/src/hello_world/load.clj
+++ hello-world/service/src/hello_world/load.clj
@@ -4,16 +4,6 @@
             [hypercrud.server.datomic.root-init :as root-init]))
 
 
-(defn load-root [uri]
-  (let [schema (-> (io/resource "root/schema.edn") slurp read-string)
-        data [(root-init/generate-db-record "samples-blog")
-              ; the only reason this root db-record exists in this loader is to satisfy init-datomic
-              ; it can be deleted when hypercrud.server.db-root/root-id is
-              (root-init/generate-db-record "root")]
-        conn (d/connect uri)]
-    @(d/transact conn schema)
-    @(d/transact conn data)))
-
 (defn load-samples-blog [uri]
   (let [schema (-> (io/resource "samples-blog/schema.edn") slurp read-string)
         data (-> (io/resource "samples-blog/data.edn") slurp read-string)
@@ -22,14 +12,11 @@
     @(d/transact conn data)))
 
 (defn initialize [transactor-uri]
-  (let [root-uri (str transactor-uri "root")]
-    (println "Loading root")
-    (when-not (d/create-database root-uri)
-      (throw (Error. "root db already exists")))
-    (load-root root-uri))
-
   (let [samples-blog-uri (str transactor-uri "samples-blog")]
     (println "Loading samples-blog")
     (when-not (d/create-database samples-blog-uri)
       (throw (Error. "samples-blog db already exists")))
-    (load-samples-blog samples-blog-uri)))
+    (load-samples-blog samples-blog-uri))
+
+  (let [root-schema (-> (io/resource "root/schema.edn") slurp read-string)]
+    (root-init/init transactor-uri root-schema)))
